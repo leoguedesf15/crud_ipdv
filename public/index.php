2e9b90d509c4e5b8a9b7c7943ece738490aeb922
Leonardo <leoguedesf15@gmail.com>
2021-09-28 12:37:05 -0300

    disable autenticacao(cors)

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
+    // Driblando CORS
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding, Referer, sec-ch-ua, sec-ch-ua-mobile,sec-ch-ua-platform,User-Agent");
+
 use app\route\RouteController;
 
 include_once '../app/inc/bootstrap.php';
-
-
     $url= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $route = RouteController::matchRoute($url,$_SERVER["REQUEST_METHOD"]);
     if(isset($route)){
